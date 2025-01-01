88f856d61772e084b851fe2dcac3f216995ca724
pirappa biradar <pirappa.biradar2013@gmail.com>
2025-01-01 23:53:20 +0530

    understanding branch mechanism

---
+++ git.py
@@ -0,0 +1,3 @@
+#git branch- this command is used to list out branches.
+#git branch branch_name- this command is used to create a new branch.
+#git checkout branch_name- this command is used to switch from one branch to another branch.
